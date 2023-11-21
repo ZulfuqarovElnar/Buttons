3561c45c95fb8481a4109a82665e119de6a62c1d
Elnar Zulfuqarli <elnarzulfuqarli2001@gmail.com>
2023-11-21 16:28:16 +0400

    Mouse olaylari

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './assets/tailwind.css'
 import Click from './components/click';
 import ClickLoading from './components/clickLoading';
+import Mouse from './components/mouse';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App grid px-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
       <Click />
       <ClickLoading />
+      <Mouse />
     </div>
   );
 }
